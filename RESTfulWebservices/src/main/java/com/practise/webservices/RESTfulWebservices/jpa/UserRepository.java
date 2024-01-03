ccb6d55105dd36b11de4cd28f5d0e97f68ccf557
anirudh-1729 <79279048+anirudh-1729@users.noreply.github.com>
2024-01-03 14:47:24 +0530

    Delete RESTfulWebservices directory

--- RESTfulWebservices/src/main/java/com/practise/webservices/RESTfulWebservices/jpa/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practise.webservices.RESTfulWebservices.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.practise.webservices.RESTfulWebservices.Users.user;
-
-public interface UserRepository extends JpaRepository<user,Integer>{
-
-}
